f9a37c340ef4c4019988f0efd27e2c0b4c714a64
Osvair Junior <osvair.valle@fatec.sp.gov.br>
2022-07-11 22:09:59 -0300

    Evento de click e estilização

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero.model';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent {
   hero: Hero = {
     id: 1,
     name: 'Batman'
   };
 
-  constructor() { }
+  heroes = HEROES;
+  selectedHero?: Hero;
 
-  ngOnInit(): void {
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
   }
-
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from "./hero.model";
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Batimão'},
+  { id: 2, name: 'Home de ferro'},
+  { id: 3, name: 'Super Home'},
+  { id: 4, name: 'Frexi'},
+  { id: 5, name: 'Molier Maravilhosa'},
+  { id: 6, name: 'Ruqui'},
+  { id: 7, name: 'Tor'},
+  { id: 8, name: 'Home Aranho'},
+];
